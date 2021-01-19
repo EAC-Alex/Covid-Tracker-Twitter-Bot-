ab6d0be8753c2a0a2b2d248be8ce3e7b3e07cad9
Henrotte Alexandre <alexandre.henrotte1@gmail.com>
2021-01-19 12:38:37 +0100

    Setup API twitter keys for project

--- covid-tracker.js
+++ covid-tracker.js
@@ -3,11 +3,6 @@
 const TwitterBot = require('./TwitterBot.js');
 const DataGetter = require('./DataGetter.js');
 
-// Schedule main function
-
-var schedule = require('node-schedule');
-var everyDayPlanner = schedule.scheduleJob('0 24 23 * * *', main);
-
 // Objects instanciation
 
 let twitterBot = new TwitterBot();
@@ -15,17 +10,6 @@ let dataGetter = new DataGetter();
 
 // Main
 
-function main() {
-    dataGetter.updateData();
-
-    values = {
-        totalCases : dataGetter.getData_totalCases(),
-        newCases : dataGetter.getData_newCases(),
-        totalDeaths: dataGetter.getData_totalDeaths(),
-        newDeaths : dataGetter.getData_newDeaths()
-    }
-
-    twitterBot.tweet(values);
-}
-
-main();
+dataGetter.updateData();
+var dataToTweet = dataGetter.getData();
+twitterBot.tweet(dataToTweet);
