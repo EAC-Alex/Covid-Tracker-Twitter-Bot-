b3d02b0811e6415837ff8b6c1195e0782a190162
Alexandre Henrotte <alexandre.henrotte1@gmail.com>
2021-02-15 00:08:52 +0100

    New functions in DatabaseManager
    Add connect(), disconnect() and getAllDocumentsBetweenDate()
    
    The last one is to select documents in the database that has the "date" propriety value between to specific dates

--- DatabaseManager.js
+++ DatabaseManager.js
@@ -4,13 +4,29 @@ const MongoClient = require('mongodb').MongoClient;
 
 class DatabaseManager {
 
-
-
     constructor() {
         this.database_uri = `mongodb+srv://Alexandre:${process.env.MONGODB_PASSWORD}@cluster0.h4zyz.mongodb.net/belgium-covid-tracker?retryWrites=true&w=majority`;
         this.databaseClient = new MongoClient(this.database_uri, { useNewUrlParser: true });
     }
 
+    connect() {
+        return new Promise(async (successCallback, failureCallback) => {
+            try {
+                await this.databaseClient.connect();
+                successCallback("Connected to mongodb database");
+            } catch (err) {
+                failureCallback(err);
+            }
+        })
+    }
+
+    disconnect() {
+        try {
+            this.databaseClient.close();
+        } catch (err) {
+            console.log(err.stack);
+        }
+    }
 
     selectDatabase(databaseName) {
         const database = this.databaseClient.db(databaseName);
@@ -24,11 +40,29 @@ class DatabaseManager {
 
     async insertValue(databaseName, collectionName, value) {
         try {
-            await this.databaseClient.connect();
             const database = this.selectDatabase(databaseName);
             const collection = this.selectCollection(database, collectionName);
             const insertFeedBack = await collection.insertOne(value);
             console.log(insertFeedBack);
+        } catch (err) {
+            console.log(err.stack);
+        }
+    }
+
+    async getAllDocumentsBetweenDates(databaseName, collectionName, dateStart, dateEnd) {
+        try {
+            const database = this.selectDatabase(databaseName);
+            const collection = this.selectCollection(database, collectionName);
+
+            var documents = await collection.find({
+                "date": {
+                    '$gte': dateStart,
+                    '$lt': dateEnd
+                }
+            }).toArray();
+            console.log(documents)
+            return documents;
+
         } catch (err) {
             console.log(err.stack);
         }
