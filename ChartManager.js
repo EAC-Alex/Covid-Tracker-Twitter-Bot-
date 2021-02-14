2356476cb784e4a91a06bb29ff347b20c630c7a1
Alexandre Henrotte <alexandre.henrotte1@gmail.com>
2021-02-14 21:47:26 +0100

    downloadChart function added

--- ChartManager.js
+++ ChartManager.js
@@ -1,10 +1,25 @@
 const QuickChart = require('quickchart-js');
+const fs = require('fs');
+const request = require('request');
 
 class ChartManager {
 
     constructor() {
         this.chart = new QuickChart();
         this.chartSize = { width: 800, height: 600 }
+    }
+
+    setSizeChart() {
+        this.chart.setWidth(this.chartSize.width);
+        this.chart.setHeight(this.chartSize.height);
+    }
+
+    getChartUrl() {
+        return this.chart.getUrl();
+    }
+
+    downloadChart() {
+        this.chart.toFile('chart.png');
     }
 
     createChart() {
@@ -17,15 +32,8 @@ class ChartManager {
                 ]
             }
         });
-    }
 
-    setSizeChart() {
-        this.chart.setWidth(this.chartSize.width);
-        this.chart.setHeight(this.chartSize.height);
-    }
-
-    getChartUrl() {
-        return this.chart.getUrl();
+        this.setSizeChart();
     }
 }
 
