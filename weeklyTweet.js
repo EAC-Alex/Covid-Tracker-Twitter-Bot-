abc27859b4e4c1ef1d4de6cd95c4cea916b71bfb
Alexandre Henrotte <alexandre.henrotte1@gmail.com>
2021-02-15 20:24:44 +0100

    Complete main steps in weeklyTweet script

--- weeklyTweet.js
+++ weeklyTweet.js
@@ -1,6 +1,6 @@
 // Class import
 const TwitterBot = require('./TwitterBot.js');
-const DataGetter = require('./DataGetter.js');
+const StatisticsManager = require('./StatisticsManager.js');
 const ChartManager = require('./ChartManager.js');
 
 // utils function import
@@ -9,21 +9,19 @@ const sleep = require('./utils/sleep.js');
 
 // Objects instanciation
 let twitterBot = new TwitterBot();
-let dataGetter = new DataGetter();
+let statisticsManager = new StatisticsManager();
 let chartManager = new ChartManager();
 
-// Update and retrieve data
-dataGetter.updateData();
-var dataCovid = dataGetter.getData();
-
-// Create the statistics chart and download it locally
-chartManager.createChart();
-chartManager.downloadChart().then(async () => {
-
-    // Wait 5 seconds for the statistics chart to be written on the file system
-    await sleep(5000);
-    // Tweet the statistics chart
-    var date = getFormattedDate();
-    twitterBot.tweetMedia(`chart_${date}.png`);
-
-});
+// Get week statistics
+statisticsManager.getWeekStatistics("newCases")
+    .then((weekStatistics => {
+        // Create the statistics chart and download it locally
+        chartManager.createChart("Nombre de nouveaux cas recensés", weekStatistics);
+        chartManager.downloadChart();
+    })).then(async () => {
+        // Wait 5 seconds for the statistics chart to be written on the file system
+        await sleep(5000);
+        // Tweet the statistics chart
+        var date = getFormattedDate();
+        twitterBot.tweetMedia(`chart_${date}.png`);
+    })
