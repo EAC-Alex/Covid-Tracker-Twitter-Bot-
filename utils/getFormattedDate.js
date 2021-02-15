92f45fbdab39a7b561c465b819c2be2db3081df9
Alexandre Henrotte <alexandre.henrotte1@gmail.com>
2021-02-15 21:35:31 +0100

    Update getFormattedDate.js

--- utils/getFormattedDate.js
+++ utils/getFormattedDate.js
@@ -9,7 +9,7 @@ function getFormattedDate() {
     if (day.length < 2) day = '0' + day;
     if (month.length < 2) month = '0' + month;
 
-    return `${year}-${month}-${day}`;
+    return `${day}-${month}-${year}`;
 }
 
 module.exports = getFormattedDate;
