5b651511c5393da9dd50041662914993da3cffc0
Alexandre Henrotte <51267593+alexandreHenrotte@users.noreply.github.com>
2021-03-26 17:37:46 +0100

    Develop (#14)
    * Check for empty data from data source
    
    * Handle empty data before tweets
    
    * Change line charts to bar charts
    
    * Remove unnecessary function call
    
    * Create new type of charts tweet
    
    * Move statistics insertion
    
    * Pass tweet title as argument for tweetMedia
    
    * Visual text changes
    
    * Add the "data date" in the objects
    
    The "data date" is the date  where the datas have been recorded and measured
    
    * Feature days since containment (#13)
    
    * Create tweetDaysSinceFirstContainment function
    
    * Create tweet that tells the number of days since first containment

---
+++ src/Tweets/daysSinceFirstContainment.js
@@ -0,0 +1,8 @@
+// Class import
+const TwitterBot = require('../TwitterBot/TwitterBot.js');
+
+// Objects instanciation
+let twitterBot = new TwitterBot();
+
+// Tweet the data
+twitterBot.tweetDaysSinceFirstContainment();
